e04b8908183ccddaf5fed170e6a951b49885b08c
Nitash <Nitash>
2016-07-29 16:06:23 -0700

    School Application

---
+++ app/src/main/java/comn/example/admin/newgenerationschool/SCHOOL.java
@@ -0,0 +1,14 @@
+package comn.example.admin.newgenerationschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SCHOOL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school);
+    }
+}
